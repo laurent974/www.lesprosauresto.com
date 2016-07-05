367370346869cb411dcbd884b985789aff8179e4
cow <cow@localhost.localdomain>
2016-07-05 18:03:22 +0400

    Menu dynamique

--- index.php
+++ index.php
@@ -13,29 +13,31 @@
   </head>
   <body>
     <main class="Global">
-      	<header class="Header slide">     <!--	Add "slideRight" class to items that move right when viewing Nav Drawer  -->
-          <ul id="navToggle" class="burger slide">    <!--	Add "slideRight" class to items that move right when viewing Nav Drawer  -->
-      			<li></li><li></li><li></li>
-      		</ul>
-      	</header><!-- /.Header -->
+      <header class="Header slide">     <!--	Add "slideRight" class to items that move right when viewing Nav Drawer  -->
+        <ul id="navToggle" class="burger slide">    <!--	Add "slideRight" class to items that move right when viewing Nav Drawer  -->
+          <li></li><li></li><li></li>
+        </ul>
+      </header><!-- /.Header -->
 
-      	<nav class="Navbar slide">
-      		<ul>
-      			<li><a href="../trunk.html" class="active">HOME</a></li>
-      			<li><a href="#">LINK TWO</a></li>
-      			<li><a href="#">LINK THREE</a></li>
-      			<li><a href="#">LINK FOUR</a></li>
-      			<li><a href="#">LINK FIVE</a></li>
-      			<li><a href="#">LINK SIX</a></li>
-      			<li><a href="#">LINK SEVEN</a></li>
-      		</ul>
-      	</nav><!-- /.Navbar -->
+      <nav class="Navbar slide">
+        <ul>
+          <li><a href="../trunk.html" class="active">HOME</a></li>
+          <li><a href="#">LINK TWO</a></li>
+          <li><a href="#">LINK THREE</a></li>
+          <li><a href="#">LINK FOUR</a></li>
+          <li><a href="#">LINK FIVE</a></li>
+          <li><a href="#">LINK SIX</a></li>
+          <li><a href="#">LINK SEVEN</a></li>
 
-      	<div class="Content slide">     <!--	Add "slideRight" class to items that move right when viewing Nav Drawer  -->
-          <div class="bloc">
-            
-          </div>
-      	</div><!-- /.Content -->
+          <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
+        </ul>
+      </nav><!-- /.Navbar -->
+
+      <div class="Content slide">     <!--	Add "slideRight" class to items that move right when viewing Nav Drawer  -->
+        <div class="bloc">
+
+        </div>
+      </div><!-- /.Content -->
     </main><!-- /.Global -->
     <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/public/js/all.js"></script>
   </body>
