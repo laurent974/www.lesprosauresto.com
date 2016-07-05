8ba1a2a5752b48095afa311d7c79300a7942afdc
cow <cow@localhost.localdomain>
2016-07-05 13:24:27 +0400

    Ajout des appels de scripts et css + meta

--- index.php
+++ index.php
@@ -3,8 +3,13 @@
   <head>
     <meta charset="utf-8">
     <title>Les pros au resto</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/public/css/main.css" media="screen" title="no title" charset="utf-8">
   </head>
   <body>
-    okok
+    <main class="Global">
+
+    </main>
+    <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/public/js/all.js"></script>
   </body>
 </html>
