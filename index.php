6e8e6efea136d8aefd9d65b2bfceba1c886f5684
cow <cow@localhost.localdomain>
2016-07-05 15:08:23 +0400

    Html Menu responsive

--- index.php
+++ index.php
@@ -4,12 +4,39 @@
     <meta charset="utf-8">
     <title>Les pros au resto</title>
     <meta name="viewport" content="width=device-width,initial-scale=1">
+
+    <!-- [if lt IE 9]
+    <script src="/public/js/html5shiv.js"></script>
+    <![endif]-->
+
     <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/public/css/main.css" media="screen" title="no title" charset="utf-8">
   </head>
   <body>
     <main class="Global">
+      	<header class="Header slide">     <!--	Add "slideRight" class to items that move right when viewing Nav Drawer  -->
+          <ul id="navToggle" class="burger slide">    <!--	Add "slideRight" class to items that move right when viewing Nav Drawer  -->
+      			<li></li><li></li><li></li>
+      		</ul>
+      	</header><!-- /.Header -->
 
-    </main>
+      	<nav class="Navbar slide">
+      		<ul>
+      			<li><a href="../trunk.html" class="active">HOME</a></li>
+      			<li><a href="#">LINK TWO</a></li>
+      			<li><a href="#">LINK THREE</a></li>
+      			<li><a href="#">LINK FOUR</a></li>
+      			<li><a href="#">LINK FIVE</a></li>
+      			<li><a href="#">LINK SIX</a></li>
+      			<li><a href="#">LINK SEVEN</a></li>
+      		</ul>
+      	</nav><!-- /.Navbar -->
+
+      	<div class="Content slide">     <!--	Add "slideRight" class to items that move right when viewing Nav Drawer  -->
+          <div class="bloc">
+            
+          </div>
+      	</div><!-- /.Content -->
+    </main><!-- /.Global -->
     <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/public/js/all.js"></script>
   </body>
 </html>
