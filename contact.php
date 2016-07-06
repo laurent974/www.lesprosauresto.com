480e92f1cb53eebaf6528777fbd39eaa2032790d
cow <cow@localhost.localdomain>
2016-07-07 01:12:28 +0400

    Ajout template contact et page

---
+++ contact.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Template Name: Contact
+*/
+?>
+<?php get_header(); ?>
+<div class="Content slide">     <!--	Add "slideRight" class to items that move right when viewing Nav Drawer  -->
+
+</div><!-- /.Content -->
+<?php get_footer(); ?>
