91932696b48c93067ddb7167e1f40a1b543ebc73
cow <cow@localhost.localdomain>
2016-07-07 01:25:18 +0400

    Update index.php pour voir les posts

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php get_header(); ?>
 <div class="Content slide">     <!--	Add "slideRight" class to items that move right when viewing Nav Drawer  -->
-  <div class="bloc">
-
-  </div>
+  <?php query_posts('category_name=Accueil'); ?>
 </div><!-- /.Content -->
 <?php get_footer(); ?>
