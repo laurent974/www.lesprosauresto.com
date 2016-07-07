203eca1da2dbeb3a889c385d55fe7abaab3eb811
cow <cow@localhost.localdomain>
2016-07-07 14:05:46 +0400

    Modification de la div row dans le template

--- index.php
+++ index.php
@@ -1,15 +1,17 @@
 <?php get_header(); ?>
-<section class="Part slide row">
+<section class="Part slide">
   <?php query_posts('category_name=Accueil'); ?>
   <?php if (have_posts()) : ?>
-    <?php while (have_posts()) : the_post(); ?>
-      <article class="Post twelve columns">
-        <h1 class="Post-Title Title-H1"><?php the_title(); ?></h1>
-        <p class="Post-Content">
-          <?php the_content(); ?>
-        </p><!-- /.Post-Content -->
-      </article><!-- /.Post -->
-    <?php endwhile; ?>
+    <div class="row">
+      <?php while (have_posts()) : the_post(); ?>
+        <article class="Post twelve columns">
+          <h1 class="Post-Title Title-H1"><?php the_title(); ?></h1>
+          <p class="Post-Content">
+            <?php the_content(); ?>
+          </p><!-- /.Post-Content -->
+        </article><!-- /.Post -->
+      <?php endwhile; ?>
+    </div><!-- /.row -->
   <?php endif; ?>
 </section><!-- /.Content -->
 <?php get_footer(); ?>
--- page.php
+++ page.php
@@ -1,12 +1,14 @@
 <?php get_header(); ?>
-<section class="Part slide row">
+<section class="Part slide">
   <?php if (have_posts()) : ?>
-    <?php while (have_posts()) : the_post(); ?>
-       <article class="Post twelve columns">
-         <h1 class="Post-Title"><?php the_title(); ?></h1>
-         <p class="Post-Content"> <?php the_content(); ?> </p>
-       </article>
-     <?php endwhile; ?>
+    <div class="row">
+      <?php while (have_posts()) : the_post(); ?>
+         <article class="Post twelve columns">
+           <h1 class="Post-Title"><?php the_title(); ?></h1>
+           <p class="Post-Content"> <?php the_content(); ?> </p>
+         </article>
+       <?php endwhile; ?>
+     </div><!-- /.row -->
    <?php endif; ?>
 </section><!-- /.Content -->
 <?php get_footer(); ?>
