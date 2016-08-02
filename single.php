9053a15a4452a6d7c1f8046f04d6ffec49a3981c
cow <cow@localhost.localdomain>
2016-08-03 00:46:39 +0400

    Update div architecture

--- single.php
+++ single.php
@@ -1,21 +1,24 @@
 <?php get_header(); ?>
   <section class="Part slide">
+    <div class="row">
     <?php
     	// Start the Loop.
     	while ( have_posts() ) : the_post(); ?>
-      <h1 class="Post-Title Title-H1"><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><i class="material-icons">&#xE56C;</i><span><?php the_title(); ?></span></a></h1>
-      <?php
-    		 the_content();
+      <article class="Post twelve columns">
+        <h1 class="Post-Title Title-H1"><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><i class="material-icons">&#xE56C;</i><span><?php the_title(); ?></span></a></h1>
+        <?php
+      		 the_content();
 
-    		// If comments are open or we have at least one comment, load up the comment template.
-    		if ( comments_open() || get_comments_number() ) {
-    			comments_template();
-    		}
-    	endwhile;
-    ?>
+      		// If comments are open or we have at least one comment, load up the comment template.
+      		if ( comments_open() || get_comments_number() ) {
+      			comments_template();
+      		} ?>
+        </article>
+    	<?php endwhile; ?>
 
     <?php if ( has_post_thumbnail() ) { ?>
         <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail('medium'); ?></a>
     <?php } ?>
+  </div>
   </section>
 <?php get_footer(); ?>
