8561527b907c3f234e30ca7b8b909acd20997063
cow <cow@localhost.localdomain>
2016-08-03 12:10:12 +0400

    Update classes img

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
           <h1 class="Post-Title Title-H1"><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><i class="material-icons">&#xE56C;</i><span><?php the_title(); ?></span></a></h1>
           <div class="Post-Content">
             <?php if ( has_post_thumbnail() ) { ?>
-                <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail('medium'); ?></a>
+                <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail('medium', array( 'class'	=> "six columns")); ?></a>
             <?php } ?>
             <?php the_content(); ?>
           </div><!-- /.Post-Content -->
