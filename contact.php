69c87e980a22bcf33d1c631b4dda8c32803323a2
cow <cow@localhost.localdomain>
2016-08-04 21:00:10 +0400

    Update Contact page

--- contact.php
+++ contact.php
@@ -4,7 +4,18 @@ Template Name: Contact
 */
 ?>
 <?php get_header(); ?>
-<div class="Content slide">     <!--	Add "slideRight" class to items that move right when viewing Nav Drawer  -->
-
-</div><!-- /.Content -->
+  <div class="Content slide">     <!--	Add "slideRight" class to items that move right when viewing Nav Drawer  -->
+    <section class="Part slide">
+      <?php if (have_posts()) : ?>
+        <div class="row">
+          <?php while (have_posts()) : the_post(); ?>
+             <article class="Post twelve columns">
+               <h1 class="Post-Title Title-H1"><i class="material-icons">&#xE2C7;</i><span><?php the_title(); ?></span></h1>
+               <div class="Post-Content"> <?php the_content(); ?> </div>
+             </article>
+           <?php endwhile; ?>
+         </div><!-- /.row -->
+       <?php endif; ?>
+    </section><!-- /.Content -->
+  </div><!-- /.Content -->
 <?php get_footer(); ?>
