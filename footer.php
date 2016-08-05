53577853901975bd75894c861f38be42377c970e
cow <cow@localhost.localdomain>
2016-08-05 20:30:24 +0400

    Update footer tag

--- footer.php
+++ footer.php
@@ -1,3 +1,6 @@
+      <footer class="Footer">
+
+      </footer>
     </main><!-- /.Global -->
   <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/public/js/all.js"></script>
   <?php wp_footer(); ?>
