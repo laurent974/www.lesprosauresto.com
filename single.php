7f0765dff5c7141be8a0e925282af119b60e7462
cow <cow@localhost.localdomain>
2016-08-12 21:42:54 +0400

    Update Thumb background

--- single.php
+++ single.php
@@ -4,20 +4,16 @@
     	// Start the Loop.
     	while ( have_posts() ) : the_post(); ?>
 
-      <?php $thumbnail = '';
-      // Get the ID of the post_thumbnail (if it exists)
-      $post_thumbnail_id = get_post_thumbnail_id($post->ID);
-      // if it exists
-      if ($post_thumbnail_id) {
-      $size = apply_filters('post_thumbnail_size', 'post-thumbnail');
-      $thumbnail = wp_get_attachment_image($post_thumbnail_id, $size, false, '');
-      }
-      if (!empty($thumbnail)) { ?>
-      <div class="Part-SingleThumbnail" style="background: url('<?php echo $thumbnail; ?>') no-repeat;">
-      <?php } else { ?>
-      <div class="Part-NoSingleThumbnail">
-        <?php } ?>
-      </div>
+      <?php
+        if (has_post_thumbnail()) {
+          $thumbnail_data = wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'my-fun-size' );
+          $thumbnail_url = $thumbnail_data[0];
+      ?>
+
+        <div class="Part-SingleThumbnail" style="background-image:url('<?php echo $thumbnail_url ?>')"></div>
+
+      <?php } ?>
+
       <div class="row">
       <article class="Post twelve columns">
         <h1 class="Post-Title Title-H1"><i class="material-icons">&#xE56C;</i><span><?php the_title(); ?></span></h1>
