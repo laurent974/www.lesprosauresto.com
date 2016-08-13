989196433f3be17b351b34da988825d9c2f57279
cow <cow@localhost.localdomain>
2016-08-13 14:48:28 +0400

    Update column commentary

--- single.php
+++ single.php
@@ -22,14 +22,16 @@
           ?>
         </article>
       </div>
-      
-      <div class="Commentary">
-        <?php
-          // If comments are open or we have at least one comment, load up the comment template.
-          if ( comments_open() || get_comments_number() ) {
-            comments_template();
-          }
-        ?>
+
+      <div class="row">
+        <div class="Commentary twelve columns">
+          <?php
+            // If comments are open or we have at least one comment, load up the comment template.
+            if ( comments_open() || get_comments_number() ) {
+              comments_template();
+            }
+          ?>
+        </div>
       </div>
   	<?php endwhile; ?>
   </section>
