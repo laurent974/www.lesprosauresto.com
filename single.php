ebfe376aca67352cf9d5965cd6579e1782e13d34
cow <cow@localhost.localdomain>
2016-08-13 13:58:16 +0400

    Update Commentary system

--- single.php
+++ single.php
@@ -20,15 +20,15 @@
         <?php
       		the_content();
         ?>
-        <div class="Post-Comment">
-          <?php
-            // If comments are open or we have at least one comment, load up the comment template.
-            if ( comments_open() || get_comments_number() ) {
-              comments_template();
-            }
-          ?>
-        </div>
       </article>
+      <div class="Commentary">
+        <?php
+          // If comments are open or we have at least one comment, load up the comment template.
+          if ( comments_open() || get_comments_number() ) {
+            comments_template();
+          }
+        ?>
+      </div>
   	<?php endwhile; ?>
 </div>
   </section>
