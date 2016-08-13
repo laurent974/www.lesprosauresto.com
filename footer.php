fb7d716ac8e8bd816a0727aed8fd23d5b7f2ae76
cow <cow@localhost.localdomain>
2016-08-13 17:11:23 +0400

    Update footer row

--- footer.php
+++ footer.php
@@ -1,5 +1,7 @@
       <footer class="Footer">
-        <span class="Color--Gold">Créé avec</span> <span class="Color--Red"><i class="material-icons">&#xE87D;</i></span>
+        <div class="row">
+          <span class="Color--Gold">Créé avec</span> <span class="Color--Red"><i class="material-icons">&#xE87D;</i></span>
+        </div>
       </footer>
     </main><!-- /.Global -->
   <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/public/js/all.js"></script>
