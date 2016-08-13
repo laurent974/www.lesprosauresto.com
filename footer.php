14eb7cb17de5de23ead54536252605706c2cb2c3
cow <cow@localhost.localdomain>
2016-08-13 17:09:22 +0400

    Update footer

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
       <footer class="Footer">
-
+        <span class="Color--Gold">Créé avec</span> <span class="Color--Red"><i class="material-icons">&#xE87D;</i></span>
       </footer>
     </main><!-- /.Global -->
   <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/public/js/all.js"></script>
