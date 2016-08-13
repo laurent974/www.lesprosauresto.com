72b17c70c97627171703dfb35a3095855aca78c3
cow <cow@localhost.localdomain>
2016-08-13 17:24:24 +0400

    Update Contact form

--- contact.php
+++ contact.php
@@ -10,8 +10,8 @@ Template Name: Contact
         <div class="row">
           <?php while (have_posts()) : the_post(); ?>
              <article class="Post twelve columns">
-               <h1 class="Post-Title Title-H1"><i class="material-icons">&#xE2C7;</i><span><?php the_title(); ?></span></h1>
-               <div class="Post-Content"> <?php the_content(); ?> </div>
+               <h1 class="Post-Title Title-H1"><i class="material-icons">&#xE8AF;</i><span><?php the_title(); ?></span></h1>
+               <div class="Post-Content Post-Contact"> <?php the_content(); ?> </div>
              </article>
            <?php endwhile; ?>
          </div><!-- /.row -->
