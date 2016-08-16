7ed34028784cdee38aba0727fb2e823ff00c529f
Yen-Pon Laurent <lyenpon974@gmail.com>
2016-08-16 13:31:43 +0400

    Update suppression de jquery dans le header

--- functions.php
+++ functions.php
@@ -7,4 +7,29 @@ add_action( 'init', 'register_my_menu' );
 if ( function_exists( 'add_theme_support' ) ) {
   add_theme_support( 'post-thumbnails' );
 }
+
+// Supprimer jquery UI
+
+wp_deregister_script( 'jquery-ui-core' );
+wp_deregister_script( 'jquery-ui-tab' );
+wp_deregister_script( 'jquery-ui-autocomplete' );
+wp_deregister_script( 'jquery-ui-accordion' );
+wp_deregister_script( 'jquery-ui-autocomplete' );
+wp_deregister_script( 'jquery-ui-button' );
+wp_deregister_script( 'jquery-ui-datepicker');
+wp_deregister_script( 'jquery-ui-dialog' );
+wp_deregister_script( 'jquery-ui-draggable' );
+wp_deregister_script( 'jquery-ui-droppable' );
+wp_deregister_script( 'jquery-ui-mouse' );
+wp_deregister_script( 'jquery-ui-position' );
+wp_deregister_script( 'jquery-ui-progressbar');
+wp_deregister_script( 'jquery-ui-resizable' );
+wp_deregister_script( 'jquery-ui-selectable');
+wp_deregister_script( 'jquery-ui-slider' );
+wp_deregister_script( 'jquery-ui-sortable' );
+wp_deregister_script( 'jquery-ui-tabs' );
+wp_deregister_script( 'jquery-ui-widget' );
+
+//Supprimer jquery
+wp_deregister_script( 'jquery' );
 ?>
