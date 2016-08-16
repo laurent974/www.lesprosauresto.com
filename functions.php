b4797a0ec80d619bf3c1d5f9b97dda80b0c2e0b5
Yen-Pon Laurent <lyenpon974@gmail.com>
2016-08-16 14:15:17 +0400

    reset appels js dans function.php

--- functions.php
+++ functions.php
@@ -7,20 +7,4 @@ add_action( 'init', 'register_my_menu' );
 if ( function_exists( 'add_theme_support' ) ) {
   add_theme_support( 'post-thumbnails' );
 }
-
-wp_enqueue_script(
-    'customjs',                                 //slug
-    '', //path
-    array('jquery'),                                      //dependencies
-    false,                                                //version
-    true                                                  //footer
-);
-
-
-function my_assets() {
-	wp_deregister_script( 'jquery' );
-	wp_register_script( 'jquery', get_template_directory_uri() . '/public/js/all.js' );
-}
-
-add_action( 'wp_enqueue_scripts', 'my_assets' );
 ?>
